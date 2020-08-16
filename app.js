54ef514fd4b00d59f3bc1f773aecc74820fd296d
arigata9 <56773132+arigata9@users.noreply.github.com>
2020-08-16 21:35:31 +0200

    Added logging functionality

--- app.js
+++ app.js
@@ -2,17 +2,22 @@ const bouncy = require('bouncy');
 
 var router = bouncy(function (req, res, bounce) {
     if(req.headers.host === 'arigata9.de') {
+        console.log('<arigata9.de> Rerouting to 3001...');
         bounce(3001);
     }
     else if(req.headers.host === 'bot.arigata9.de') {
+        console.log('<bot.arigata9.de> Rerouting to 3002...');
         bounce(3002);
     }
     else if(req.headers.host === 'cdn.arigata9.de') {
+        console.log('<cdn.arigata9.de> Rerouting to 3003...');
         bounce(3003);
     }
     else {
+        console.log('Route <'+req.headers.host+'> couldnt be handled.');
         res.statusCode = 404;
         res.end('no such domain');
     }
 });
 router.listen(3000);
+console.log('Router started at 3000');
